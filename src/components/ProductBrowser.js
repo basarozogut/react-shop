793abf9f7476b0c70935aa29fea545876f65ef49
Basar Ozogut <5684602+basarozogut@users.noreply.github.com>
2022-03-15 19:11:35 +0300

    replace manual api call with useApiCall hook

--- src/components/ProductBrowser.js
+++ src/components/ProductBrowser.js
@@ -3,46 +3,33 @@ import { Col, Row } from "react-bootstrap";
 import { useSearchParams } from "react-router-dom";
 import { fetchProducts } from "../api/shopApi";
 import Product from "./Product";
+import useApiCall from "../hooks/useApiCall";
 
 export default function ProductBrowser() {
-    const [products, setProducts] = useState([]);
     let [searchParams] = useSearchParams();
     const searchInput = searchParams.get('filter');
+    const [products, error] = useApiCall(() => fetchProducts(searchInput), null, [searchInput]);
 
-    useEffect(() => {
-        let mounted = true;
-
-        (async () => {
-            try {
-                const response = await fetchProducts(searchInput);
-                if (mounted) {
-                    setProducts(response);
-                }
-            } catch (err) {
-                console.log(err);
-            }
-        })();
-
-        return () => {
-            mounted = false;
-        };
-    }, [searchInput]);
-
-    const renderProducts = () => {
-        if (products.length <= 0) {
-            return <p data-testid="no-products-warning">Sorry, no matching products found.</p>
-        }
-
-        return (
-            <Row xs={1} md={2} className="g-4">
-                {products.map((product) => (
-                    <Col md={4} key={product.id}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
-        );
+    if (error) {
+        console.log(error);
+        return <p>There was an error while trying to fetch the products.</p>;
     }
 
-    return renderProducts();
+    if (!products) {
+        return <p>Loading...</p>
+    }
+
+    if (products.length <= 0) {
+        return <p data-testid="no-products-warning">Sorry, no matching products found.</p>
+    }
+
+    return (
+        <Row xs={1} md={2} className="g-4">
+            {products.map((product) => (
+                <Col md={4} key={product.id}>
+                    <Product product={product} />
+                </Col>
+            ))}
+        </Row>
+    );
 }
