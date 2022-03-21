ceccbe7a4c023f91e4e06b4f7c82a6ac5cea6c9a
Basar Ozogut <5684602+basarozogut@users.noreply.github.com>
2022-03-22 00:49:24 +0300

    add cart total.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,11 +2,12 @@ import useCart from '../hooks/useCart';
 import './Cart.css';
 import useApiCall from "../hooks/useApiCall";
 import { fetchProductsWithId } from "../api/shopApi";
+import { Fragment } from 'react';
 
 export default function Cart() {
     const { cartItems, getCartItemById } = useCart();
     const cartItemIds = cartItems.map(item => item.id);
-    const [products, error] =  useApiCall(() => fetchProductsWithId(cartItemIds), null, [JSON.stringify(cartItemIds)]);
+    const [products, error] = useApiCall(() => fetchProductsWithId(cartItemIds), null, [JSON.stringify(cartItemIds)]);
 
     let content = null;
 
@@ -18,12 +19,18 @@ export default function Cart() {
     } else if (products.length <= 0) {
         content = <p>Cart is empty.</p>
     } else {
+        const total = products.reduce((prevProduct, product) => ({ price: prevProduct.price + product.price * getCartItemById(product.id).amount }), { price: 0 }).price;
         content =
-            (<ul>
-                {products.map(product => (
-                    <li key={product.id}>{product.title} Amount: {getCartItemById(product.id).amount}</li>
-                ))}
-            </ul>);
+            (
+                <Fragment>
+                    <ul>
+                        {products.map(product => (
+                            <li key={product.id}>{product.title} Amount: {getCartItemById(product.id).amount}</li>
+                        ))}
+                    </ul>
+                    <div className='Cart-total'><strong>Total:</strong> {total}</div>
+                </Fragment>
+            );
     }
 
     return (
