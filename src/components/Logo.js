86073a95ea6b973e80d79b429e2fee5ae01a2c9e
Basar Ozogut <5684602+basarozogut@users.noreply.github.com>
2022-03-21 23:39:13 +0300

    add logo link and animation.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,12 @@
 import './Logo.css';
 import { Fragment } from "react";
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
         <Fragment>
             <div className="Logo">
-                e-SHOP
+                <Link to="/">e-SHOP</Link>
             </div>
             <div className='Logo-subtext'>Your number one address for e-commerce.</div>
         </Fragment>
